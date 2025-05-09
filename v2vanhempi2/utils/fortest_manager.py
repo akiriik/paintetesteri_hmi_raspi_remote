acb6970826f65b5e8ae19936c68a8fa7de964673
Aki Riikonen <aki.riikonen@hawk.fi>
2025-05-09 13:53:54 +0300

    555

--- v2vanhempi2/utils/fortest_manager.py
+++ v2vanhempi2/utils/fortest_manager.py
@@ -13,8 +13,9 @@ class ForTestWorker(QObject):
             print(f"ForTest-yhteys epäonnistui: {e}")
             from utils.fortest_handler import DummyForTestHandler
             self.fortest = DummyForTestHandler()
-            # Lähetä virheviesti resultReady-signaalin kautta
-            self.resultReady.emit(False, 999, "ForTest-yhteys epäonnistui - laite ei kytkettynä")
+            # Lähetä signaali hieman viiveellä, jotta vastaanottaja ehtii yhdistää sen
+            QTimer.singleShot(0, lambda: self.resultReady.emit(False, 999, "ForTest-yhteys epäonnistui - laite ei kytkettynä"))
+
     
     @pyqtSlot()
     def start_test(self):
